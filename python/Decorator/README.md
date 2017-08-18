4346549a87f9fbc63280dfe724ab4252533ea2db
hatcherfang <hatcherfang@163.com>
2017-08-18 10:47:19 +0800

    add decorator yield function test case

---
+++ python/Decorator/README.md
@@ -0,0 +1,3 @@
+## Decorator
+### Decorate the yield function
+- My colleague ask me how to decorate the yield function. I try to write a test case (decoratorWithYield.py) and finally using yield transfer behavior to solve the problem.
---
+++ python/Decorator/decoratorWithYield.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def retry(times):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            i = 0
+            while i < times:
+                yield func(*args, **kw)
+                i = i + 1
+        return wrapper
+    return decorator
+
+
+@retry(3)
+def yieldFunc():
+    yield requests.get('http://www.python.org/')
+
+if __name__ == "__main__":
+    for i in yieldFunc():
+        print i.next().status_code
